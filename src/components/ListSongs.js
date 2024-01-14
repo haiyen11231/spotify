af2e5daf84f9fa100eafb640d55037e1f1b4a9da
haiyen11231 <haiyen11231@gmail.com>
2024-01-15 02:39:48 +0800

    finished but havenot fixed all bugs

--- src/components/ListSongs.js
+++ src/components/ListSongs.js
@@ -1,26 +1,51 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from "react";
+import { Songs } from "../Context";
 
 export default function ListSongs() {
+  const { DataSongs, handleSetSong, song } = useContext(Songs);
+  const [idSong, setIdSong] = useState(0);
+  const handleSong = (idSong) => {
+    setIdSong(idSong);
+    handleSetSong(idSong);
+  };
+  useEffect(() => {
+    setIdSong(song.id);
+  }, [song]);
+  console.log(DataSongs);
   return (
-    <div className="col-span-2">
-        <table className="table-auto w-full">
-            <thead className="text-white">
-                <tr>
-                    <th>#</th>
-                    <th className="text-left">Title</th>
-                    <th className="w-[10%]">Author</th>
-                    <th className="w-[10%]"><i className="fa fa-download"></i></th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>1</td>
-                    <td>1</td>
-                    <td>1</td>
-                </tr>
-            </tbody>
-        </table>
+    <div className="col-span-2 overflow-y-scroll">
+      <table className="table-auto w-full">
+        <thead className="text-white">
+          <tr>
+            <th className="w-[10%]">#</th>
+            <th className="text-left">Title</th>
+            <th className="w-[10%]">Author</th>
+            <th className="w-[10%]">
+              <i className="fa fa-download"></i>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {DataSongs.map((song, index) => (
+            <tr
+              key={index}
+              className={`bg-slate-800 h-12 text-gray-500 hover:bg-slate-600 ${
+                idSong === song.id && "text-teal-400 bg-slate-600"
+              }`}
+              onClick={() => handleSong(song.id)}
+            >
+              <td className="text-center">{index + 1}</td>
+              <td>{song.name}</td>
+              <td className="text-center">{song.author}</td>
+              <td className="text-center">
+                <a href={song.url}>
+                  <i className="fa fa-download"></i>
+                </a>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
-  )
+  );
 }
